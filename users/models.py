439086ce9ee4349601da82ab9f2aa1095da6cc8c
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-02-16 16:02:15 +0200

    abstract user model fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from enum import Enum
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class ClientChoice(Enum):
+    CLIENT = ("Client",)
+    PILOT = "Pilot"
+
+
+class CustomUser(AbstractUser):
+    user_type = models.CharField(
+        max_length=25, choices=[("What role are you?", 0), ("Client", 1), ("Pilot", 2)]
+    )
+    tel_number = models.CharField(max_length=15, help_text="Enter phone number")
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural  = "User"
