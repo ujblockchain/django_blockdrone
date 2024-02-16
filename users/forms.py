480ad20a19f90872d3c58fc89022a0b304c0ab4d
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-02-16 16:01:09 +0200

    custom user change and creation form

---
+++ users/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'user_type',
+            'tel_number',
+        )
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'first_name',
+            'last_name',
+            'user_type',
+            'tel_number',
+        )
