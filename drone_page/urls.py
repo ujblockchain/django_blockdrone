b0c1498474e5aeacced3c167cd359bd00cac5cdf
OTeeEnabor <tobienabor@gmail.com>
2024-04-30 08:09:14 +0200

    added class based views for the job review and request pages.

--- drone_page/urls.py
+++ drone_page/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
+from .views import JobRequest, JobReview
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("browse-pilots", views.browse_pilots, name="browse-pilots"),
     path("browse-pilots/<slug:slug>", views.pilot_detail, name="pilot-detail"),
-    path("job-review", views.job_review, name="job-review"),
-    path("job-request", views.job_request, name="job-request"),
+    path("job-review", JobReview.as_view(), name="job-review"),
+    path("job-request", JobRequest.as_view(), name="job-request"),
     path("accounts/login", views.user_login, name="login"),
     path("accounts/logout", views.user_logout, name="logout"),
     path("accounts/signup", views.sign_up, name="sign-up"),
